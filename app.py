149f71d8cc92d1e8a59d9595dc3f00e7ef1a3c29
InfinusA <aipacifico24@gmail.com>
2021-10-20 17:44:40 -0700

    More Boilerplate+Remove example code

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-__init__.py
+from flask import Flask
+import os
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'posh.it.sqlite'),
+    )
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+    os.makedirs(app.instance_path, exist_ok=True) #silently fail if it does happen to exist
+    import api, db
+    db.init_app(app)
+    app.register_blueprint(api.bp)
+
+    return app
